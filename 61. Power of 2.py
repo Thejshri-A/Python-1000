6fadefb5f36d0d2c3dc2cc104b01e403e756ee0a
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-09-23 00:03:13 +0530

    Updated 61. Power of 2.py

--- 61. Power of 2.py
+++ 61. Power of 2.py
@@ -10,17 +10,3 @@ print(isPowerOfTwo(n))  # Output: True
 
 n = 18
 print(isPowerOfTwo(n))  # Output: False
-"""
-
-def isPowerOfTwo(n):
-    if n <= 0:
-        return False
-    return (n & (n - 1)) == 0
-
-# Example Usage:
-n = 16
-print(isPowerOfTwo(n))  # Output: True
-
-n = 18
-print(isPowerOfTwo(n))  # Output: False
-"""
