190a339994641a73a9a1240d2d4fc809dc358948
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-09-29 02:26:19 +0530

    78. Triplet Sum
    TripletSum
    Given an array nums of n integers, find all unique triplets in the array that sum to zero.
    Example:
    Input: nums = [-1, 0, 1, 2, -1, -4]
    Output: [[-1, 0, 1], [-1, -1, 2]]

---
+++ 78. Triplet Sum.py
@@ -0,0 +1,32 @@
+def tripletSum(nums):
+    res = []
+    nums.sort()
+    total = 0
+    
+    for i in range(len(nums)-2):
+        
+        #Checks for duplicate entries
+        if i>0 and nums[i] == nums[i-1]:
+            continue
+        
+        left, right= i+1, len(nums) -1
+        while left<right:
+            total = nums[i] + nums[left] + nums[right]
+            
+            if total == 0:
+                res.append([nums[i], nums[left], nums[right]])
+                left +=1
+                right -=1
+                while left<right and nums[left]==nums[left+1]:
+                    left +=1
+                while left<right and nums[right]==nums[right-1]:
+                    right -=1
+        
+            elif total<0 :
+                left+=1
+            else:
+                right -=1
+    return res
+# Example
+nums = [-1, 0, 1, 2, -1, -4]
+print(tripletSum(nums))  # Output: [[-1, 0, 1], [-1, -1, 2]]
