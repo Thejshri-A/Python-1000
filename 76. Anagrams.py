93aeb477c8950131e7d88651899766399fbe463a
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-09-29 01:59:14 +0530

    76. Anagrams
    Given an array of strings, group anagrams together.

---
+++ 76. Anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    anagrams = defaultdict(list)
+    for s in strs:
+        key = ''.join(sorted(s))
+        anagrams[key].append(s)
+    return list(anagrams.values())
+
+# Example
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(group_anagrams(strs))  # Output: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
