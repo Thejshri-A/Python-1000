53ceea3c570d69006ccb2c9310c51ab74e4c7eac
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-12-05 15:30:38 +0530

    356. Is Valid Parathesis
    Determine If a String is Valid Parentheses
    Write a function to check if a string contains valid parentheses.
    Example:
    Input: s = "()[]{}"
    Output: true

---
+++ 356. Is Valid Parathesis.py
@@ -0,0 +1,17 @@
+def is_valid(s):
+    stack=[]
+    mapping={")":"(", "}":"{", "]":"["}
+    for char in s:
+        if char in mapping:
+            top=stack.pop() if stack else "#"
+            if mapping[char]!= top:
+                return False
+        else:
+            stack.append(char)
+            
+    return not stack
+
+# Example Usage
+s = "()[]{}"
+print(is_valid(s))  # Output: True
+
