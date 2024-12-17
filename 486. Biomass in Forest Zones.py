2ac64ff5ea53101d19a50e7cb603699fabc5fe3d
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-12-18 04:54:14 +0530

    486. Biomass in Forest Zones

---
+++ 486. Biomass in Forest Zones.py
@@ -0,0 +1,6 @@
+def biomass_in_forest_zones(area, biomass_density):
+    return area*biomass_density
+
+area = 200
+biomass_density = 5
+print(biomass_in_forest_zones(area, biomass_density), " tons")
