7515e7ae7cb1bbd024143db1a2130eb6d65c5634
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-12-23 22:59:25 +0530

    511. Annual Average Rainfall
    511. Calculate Average Rainfall per Month
    Given total annual rainfall and the number of months, calculate the average monthly rainfall.
    Example:
    Input: annual_rainfall = 1200, months = 12

---
+++ 511. Annual Average Rainfall.py
@@ -0,0 +1,6 @@
+def annual_average_rainfall(annual_rainfall, months):
+    return annual_rainfall/months
+
+annual_rainfall = 1200
+months = 12
+print("Average Annual Rainfall : ", annual_average_rainfall(annual_rainfall, months), "mm")
