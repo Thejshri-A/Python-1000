8e5d107998a0edf9eeda820f9b2a34ad948fd278
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2024-12-31 03:01:54 +0530

    593. Renewable Energy Payback Period

---
+++ 593. Renewable Energy Payback Period.py
@@ -0,0 +1,6 @@
+def renewable_energy_payback_period(initial_cost, annual_saving):
+    return initial_cost/annual_saving
+
+initial_cost = 10000
+annual_saving = 2000
+print(renewable_energy_payback_period(initial_cost, annual_saving))
