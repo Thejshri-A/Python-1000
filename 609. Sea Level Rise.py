9127f8ca53cbc34cb19756e93df216f86c981669
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2025-01-01 22:38:04 +0530

    609. Sea Level Rise

---
+++ 609. Sea Level Rise.py
@@ -0,0 +1,5 @@
+def sea_level_rise(volume, density_conversion_factor):
+    return volume*density_conversion_factor
+volume = 1000
+density_conversion_factor = 0.001
+print(sea_level_rise(volume, density_conversion_factor))
