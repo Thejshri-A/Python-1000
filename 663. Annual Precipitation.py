2b6a47cedca78e637ff9374b1d4e117461adadfa
Thejshri A <155169658+Thejshri-A@users.noreply.github.com>
2025-01-05 23:20:48 +0530

    663. Annual Precipitation

---
+++ 663. Annual Precipitation.py
@@ -0,0 +1,5 @@
+def annual_precipiation(precipitation):
+    return sum(precipitation)
+
+precipitation = [50, 40, 60, 70, 80, 90, 100, 50, 40, 30, 20, 10]
+print(annual_precipiation(precipitation))
